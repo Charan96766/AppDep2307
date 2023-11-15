b8dc3bb9a8926b0f4d29872a85e912984d5266dd
vajjala sai charan <vajjalasaicharan123@gmail.com>
2023-11-15 12:28:42 +0530

    made changes according to cyclic

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const bcrypt = require("bcrypt");
 app = express();
 app.use(cors()); 
 app.use('/uploads', express.static('uploads')); 
+app.use(express.static(path.join(__dirname, "./client/build")));
+
 
 
 let connectToDb = async(req,res) => {
